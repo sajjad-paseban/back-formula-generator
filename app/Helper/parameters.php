a1b6b696fd470810157e79783f429e91b4fe4ac6
sajad paseban <s.pr98@yahoo.com>
2024-03-27 04:35:26 -0700

    parameter edited

--- app/Helper/parameters.php
+++ app/Helper/parameters.php
@@ -1,9 +1,28 @@
 <?php
 
+use App\Models\Arseh;
+use Illuminate\Support\Facades\DB;
 class Parameters{
 
-    public function area($ifb, $tb): int{
-        return 32 * $ifb + $tb;
+    private $melk_id;
+
+    public function __construct($melk_id){
+        $this->melk_id = $melk_id;
+    }
+
+
+    public function sql_query($sql){
+        $result = (array) DB::selectOne($sql);
+        return $result;
+    }
+
+    public function area($ifb): int{
+        $arseh = Arseh::where('melk_id', $this->melk_id)
+        ->where('IFB', $ifb)->first();
+        
+        $area = $arseh->MasZamin;
+
+        return $area;
     }
 
     public function numberOfFloors(): int{
